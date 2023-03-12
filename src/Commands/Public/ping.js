d6a66021f8e05d27d2d4688896fb2da031bfffd0
!!Programmer!! <yonghayden68@gmail.com>
2023-03-12 14:46:36 +0800

    fix: config file

--- src/Commands/Public/ping.js
+++ src/Commands/Public/ping.js
@@ -1,12 +1,51 @@
-import { SlashCommandBuilder } from "discord.js";
+import {
+    CommandInteraction,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    Colors,
+} from "discord.js";
+import { ExtendedClient } from "../../Base/index.js";
 
 export default {
     data: new SlashCommandBuilder()
         .setName(`ping`)
         .setDescription(`Returns the latency.`),
+    /**
+     * @param {CommandInteraction} interaction
+     * @param {ExtendedClient} client
+     */
     async execute(interaction, client) {
-        interaction.reply({
-            content: `test`,
-        });
+        const now = Date.now();
+
+        const pingEmbed = new EmbedBuilder()
+            .setTitle(`Pong!`)
+            .addFields([
+                { name: `Message Ping`, value: `_Pinging..._` },
+                { name: `WebSocket`, value: `${client.ws.ping}ms` },
+            ])
+            .setColor();
+
+        interaction
+            .reply({
+                embeds: [pingEmbed],
+            })
+            .then((value) => {
+                const end = Date.now();
+                const messagePing = end - now;
+
+                pingEmbed.setFields([
+                    {
+                        name: `Message Ping`,
+                        value: `${messagePing}ms`,
+                    },
+                    { name: `WebSocket`, value: `${client.ws.ping}ms` },
+                ]);
+
+                setTimeout(() => {
+                    interaction.editReply({
+                        embeds: [pingEmbed],
+                    });
+                }, 1000);
+            });
     },
 };
